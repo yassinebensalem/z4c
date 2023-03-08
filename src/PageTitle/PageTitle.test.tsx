356155687ebe746b4935e8bb11459e7ca78b6368
farahBALI <far11114566trghc@gmail.com>
2023-03-08 17:58:00 +0100

    update component

--- src/PageTitle/PageTitle.test.tsx
+++ src/PageTitle/PageTitle.test.tsx
@@ -1,26 +1,21 @@
-// // Generated with util/create-component.js
-// import React from "react";
-// import { render } from "@testing-library/react";
-// import PageTitle from "./PageTitle";
-// import { PageTitleProps } from "./PageTitle.types";
-// import { titleStyle, divStyle } from "./styles"; 
-// describe("Test Component", () => {
-//   let props: PageTitleProps;
-//   beforeEach(() => {
-//     props = {
-//       title: "harvey was here",
-//       titleStyle:titleStyle,
-//       divStyle:divStyle
-//     };
-//   });
-//   const renderComponent = () => render(<PageTitle {...props} />);
+// Generated with util/create-component.js
+import React from "react";
+import { render } from "@testing-library/react";
+import PageTitle from "./PageTitle";
+import { PageTitleProps } from "./PageTitle.types"; 
+describe("Test Component", () => {
+  let props: PageTitleProps;
+  beforeEach(() => {
+    props = {
+      title: "harvey was here",
+    };
+  });
+  const renderComponent = () => render(<PageTitle {...props} />);
 
-//   it("should render foo text correctly", () => {
-//     props.title= "harvey was here",
-//     titleStyle,
-//     divStyle
-//     const { getByTestId } = renderComponent();
-//     const component = getByTestId("PageTitle");
-//     expect(component).toHaveTextContent("harvey was here");
-//   });
-// });
+  it("should render foo text correctly", () => {
+    props.title= "harvey was here"
+    const { getByTestId } = renderComponent();
+    const component = getByTestId("PageTitle");
+    expect(component).toHaveTextContent("harvey was here");
+  });
+});
--- src/PageTitle/PageTitle.types.ts
+++ src/PageTitle/PageTitle.types.ts
@@ -1,16 +1,16 @@
 // // Generated with util/create-component.js
 export interface PageTitleProps {
-    color:string,
-    bgColor:string;
-    fontFamily: string;
-    fontWeight: string;
-    letterSpacing: string;
-    fontSize: string;
-    height: string;
-    paddingLeft: string;
-    width: string;
-    borderRadius: string;
-    title:string
+    color?:string,
+    bgColor?:string;
+    fontFamily?: string;
+    fontWeight?: string;
+    letterSpacing?: string;
+    fontSize?: string;
+    height?: string;
+    paddingLeft?: string;
+    width?: string;
+    borderRadius?: string;
+    title?:string
 }
 // export default PageTitleProps;
 
