f126268c87f796209db4e9531917f42d4a04e018
jobran amairi <jobran41@gmail.com>
2023-03-08 22:58:51 +0100

    rename css file in strorCard

--- src/StoreCard/StoreCard.tsx
+++ src/StoreCard/StoreCard.tsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import { StoreCardProps } from './StoreCard.types';
-import { Icon } from '@iconify/react';
-import './Store.css';
+import React from "react";
+import { StoreCardProps } from "./StoreCard.types";
+import { Icon } from "@iconify/react";
+import "./StoreCard.css";
 
-
-const Store: React.FC<StoreCardProps> = ({ image, title, starIcon, rating }) => {
+const Store: React.FC<StoreCardProps> = ({
+  image,
+  title,
+  starIcon,
+  rating,
+}) => {
   return (
     <div className="store">
       <img className="store-image" src={image} alt={title} />
       <div className="store-title">
         <h2>{title}</h2>
         <div className="store-rating">
-        <p>{rating} <Icon icon="material-symbols:star"/> </p>
-        </div> 
+          <p>
+            {rating} <Icon icon="material-symbols:star" />{" "}
+          </p>
+        </div>
       </div>
     </div>
   );
 };
-
 
 export default Store;
