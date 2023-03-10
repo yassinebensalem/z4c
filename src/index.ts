eaedde2b893c5d4bcd07ad9a97060ef6102db518
jobran amairi <jobran41@gmail.com>
2023-03-10 09:57:56 +0100

    fix PageTitle type

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import TestComponent from "./TestComponent";
 import Button from "./Button";
 import Loader from "./Loader";
-import List from "./List/List";
+import List from "./List";
 import GenericWrapper from "./GenericWrapper";
 import PageTitle from "./PageTitle";
+import StoreCard from "./StoreCard";
 
 
-export { TestComponent, Button, Loader, PageTitle, GenericWrapper, List };
+export { TestComponent, Button, Loader, PageTitle, GenericWrapper, List, StoreCard };
 
